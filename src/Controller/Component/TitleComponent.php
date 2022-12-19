0d5eb1e9d4215c8308a60c3ac3150ff4eabd5726
Alexander Volle <alexander.volle@hotmail.com>
2022-12-19 20:21:54 +0100

    Update typehint

--- src/Controller/Component/TitleComponent.php
+++ src/Controller/Component/TitleComponent.php
@@ -29,7 +29,7 @@ class TitleComponent extends Component
      * - ignoreIndex : Whether to omit the controller action in the format if the requested action is index
      * - showDisplayFieldOnView : Whether to append the displayField when the requested controller action is view
      *
-     * @var array
+     * @var array<string, mixed>
      */
     protected $_defaultConfig = [
         'appName' => '',
