e948f9f35feb155d82807a6d20db0fe5a5e34860
Alexander Volle <alexander.volle@hotmail.com>
2022-12-19 20:25:51 +0100

    Fix unnecessary null coalescing operator

--- src/Controller/Component/TitleComponent.php
+++ src/Controller/Component/TitleComponent.php
@@ -120,7 +120,7 @@ class TitleComponent extends Component
             return implode(' - ', array_map(fn ($field) => (string)$entity->get($field), $displayField));
         }
 
-        return (string)$entity->get($displayField) ?? '';
+        return (string)$entity->get($displayField);
     }
 
     /**
