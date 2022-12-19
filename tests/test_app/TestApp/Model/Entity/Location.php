8d4d3819e4c4a7966754ff4a352e71a321d77ff5
Alexander Volle <alexander.volle@hotmail.com>
2022-12-19 21:02:00 +0100

    Update types

--- tests/test_app/TestApp/Model/Entity/Location.php
+++ tests/test_app/TestApp/Model/Entity/Location.php
@@ -22,9 +22,9 @@ class Location extends Entity
      * be mass assigned. For security purposes, it is advised to set '*' to false
      * (or remove it), and explicitly make individual fields accessible as needed.
      *
-     * @var array
+     * @var array<string, bool>
      */
-    protected $_accessible = [
+    protected array $_accessible = [
         'name' => true,
         'address' => true,
         'postCode' => true,
